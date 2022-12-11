6daf0120db2cead1211cee642ddf5730ae09ad15
Christoph Coijanovic <christoph.coijanovic@kit.edu>
2022-12-11 11:19:17 +0100

    It's alive! -- added tick

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use rand::prelude::*;
+use std::{thread, time};
 
 const SIZE : usize = 48;
-const CHANCE : f64 = 0.5;
+const CHANCE : f64 = 0.2;
+const SLEEP_INTERVAL : time::Duration = time::Duration::from_millis(300);
 
 /// Prints the field in my *custom aesthetic*
 fn print_field(field: &[[bool; SIZE]; SIZE]) {
@@ -33,15 +35,18 @@ fn init_random_field(field: &mut [[bool; SIZE]; SIZE]) {
 }
 
 /// Returns how many livig neighbors the poit coord has
-fn get_living_neighbor_count(coord: (u32, u32), field: &[[bool; SIZE]; SIZE]) -> u8 {
+fn get_living_neighbor_count(coord: (i32, i32), field: &[[bool; SIZE]; SIZE]) -> u8 {
     let mut count: u8 = 0;
-    for i in coord.0-1..=coord.0+1 {
-        for j in coord.1-1..=coord.1+1 {
+    for i in coord.0-1..coord.0+2 {
+        for j in coord.1-1..coord.1+2 {
+            if i < 0 || j < 0 || i >= SIZE as i32 || j >= SIZE as i32 {
+                continue;
+            }
             if (i == coord.0 && j == coord.1) || (i == coord.1 && j == coord.0) {
-                continue
+                // continue if (i,j) == coord
+                continue;
             }
             if field[i as usize][j as usize] {
-                println!("Field[{i}][{j}] is {}", field[i as usize][j as usize]);
                 count += 1;
             }
         }
@@ -49,9 +54,24 @@ fn get_living_neighbor_count(coord: (u32, u32), field: &[[bool; SIZE]; SIZE]) ->
     return count;
 }
 
-/// Evolves the given field one step
-fn step(field: &mut [[bool; SIZE]; SIZE]) {
-
+/// Evolves the given field one tick
+fn tick(field: &mut [[bool; SIZE]; SIZE]) {
+    for i in 0..SIZE-1 {
+        for j in 0..SIZE-1 {
+            let lnc: u8 = get_living_neighbor_count((i as i32, j as i32), &field);
+            if field[i][j] && (lnc == 2 || lnc == 3) {
+                // Any live cell with two or three live neighbours survives.
+                field[i][j] = true;
+            } else if !field[i][j] && lnc == 3 {
+                //Any dead cell with three live neighbours becomes a live cell.
+                field[i][j] = true;
+            } else {
+                // All other live cells die in the next generation.
+                // Similarly, all other dead cells stay dead.
+                field[i][j] = false;
+            }
+        }
+    }
 }
 
 fn main() {
@@ -61,7 +81,10 @@ fn main() {
         field[i][i] = false;
     }
     init_random_field(&mut field);
-    print_field(&field);
-    let testcount: u8 = get_living_neighbor_count((1,5), &field);
-    println!("Count: {testcount}");
+    let lnc: u8 = get_living_neighbor_count((0,0), &field);
+    loop {
+        print_field(&field);
+        tick(&mut field);
+        thread::sleep(SLEEP_INTERVAL);
+    }
 }
